eeb8332b8e9aa25975c6348c6993110001488224
KristinaMumm <56913164+KristinaMumm@users.noreply.github.com>
2021-12-16 16:33:38 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,8 @@
-const data = {"labelType":"LINE","eventSpace":200,"readMore":"Loe rohkem...","events":[{"queueNr":1,"label":"","htmlContent":"<p style=\"font-family: 'Arial';\">Insert text here</p>","packed":false,"uuid":"573f0e47-e502-4db2-8d76-508253847bff"}]}
+const data = {"labelType":"LINE","eventSpace":200,"readMore":"Loe rohkem","events":[{"queueNr":1,"label":"",
+        "htmlContent":"<p><iframe width=\"973\" height=\"547\" src=\"https://www.youtube.com/embed/3rWJ-1X_mpE\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen=\"\"></iframe></p><p style=\"font-family: 'Arial';\">Insert text here</p><p style=\"font-family: 'Arial';\"><span style=\"font-family: Arial;\">Insert text here</span></p><p style=\"font-family: 'Arial';\"><span style=\"font-family: Arial;\">Insert text here</span></p><p style=\"font-family: 'Arial';\"><span style=\"font-family: Arial;\">Insert text here</span></p><p style=\"font-family: 'Arial';\"><span style=\"font-family: Arial;\">Insert text here</span></p>"
+        ,"packed":false,"uuid":"68449fdc-918d-4a97-840d-7fbc6a9a4919"},{"queueNr":2,"label":"",
+        "htmlContent":"<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>"
+        ,"packed":false,"uuid":"68449fdc-918d-4a97-840d-7fbc6a9a4919"},{"queueNr":2,"label":"",
+        "htmlContent":"<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blal1231balba</p>,<p>Blalba313221lba</p>,<p>Bl2121albalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>"
+        ,"packed":false,"uuid":"68449fdc-918d-4a97-840d-7fbc6a9a4919"}
+    ]}
