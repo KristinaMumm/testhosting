1a231a88e1b767c628a83edd05d192376304a94d
KristinaMumm <56913164+KristinaMumm@users.noreply.github.com>
2021-12-16 16:41:03 +0200

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ const data = {"labelType":"LINE","eventSpace":200,"readMore":"Loe rohkem","event
         "htmlContent":"<p><iframe width=\"973\" height=\"547\" src=\"https://www.youtube.com/embed/3rWJ-1X_mpE\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen=\"\"></iframe></p><p style=\"font-family: 'Arial';\">Insert text here</p><p style=\"font-family: 'Arial';\"><span style=\"font-family: Arial;\">Insert text here</span></p><p style=\"font-family: 'Arial';\"><span style=\"font-family: Arial;\">Insert text here</span></p><p style=\"font-family: 'Arial';\"><span style=\"font-family: Arial;\">Insert text here</span></p><p style=\"font-family: 'Arial';\"><span style=\"font-family: Arial;\">Insert text here</span></p>"
         ,"packed":false,"uuid":"68449fdc-918d-4a97-840d-7fbc6a9a4919"},{"queueNr":2,"label":"",
         "htmlContent":"<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>"
-        ,"packed":false,"uuid":"68449fdc-918d-4a97-840d-7fbc6a9a4919"},{"queueNr":2,"label":"",
+        ,"packed":false,"uuid":"68449fdc-918d-4a97-840d-7fbc6a9a4919"},{"queueNr":3,"label":"",
         "htmlContent":"<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blal1231balba</p>,<p>Blalba313221lba</p>,<p>Bl2121albalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>,<p>Blalbalba</p>"
         ,"packed":false,"uuid":"68449fdc-918d-4a97-840d-7fbc6a9a4919"}
     ]}
